ad5bd137a96be382fc90ab2a541b1cf9661e2f80
Zhaidarbek Ayazbayev <zhaidarbek@gmail.com>
2013-02-01 20:57:43 +0600

    clean up the sample

--- src/test/java/com/groupdocs/sdk/api/EnvelopeExampleOne.java
+++ src/test/java/com/groupdocs/sdk/api/EnvelopeExampleOne.java
@@ -2,10 +2,7 @@ package com.groupdocs.sdk.api;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,11 +18,10 @@ import com.groupdocs.sdk.model.SignatureEnvelopeSettings;
 import com.groupdocs.sdk.model.SignatureFieldInfo;
 import com.groupdocs.sdk.model.SignatureRoleInfo;
 
-import com.sun.net.httpserver.*;
-
 public class EnvelopeExampleOne {
-	static String userId = "2721ad21bcf0d71e";
-	static String apiKey = "8d8a7d642a807a31c2741c101a60cef2";
+	static String userId = "16698cae909805f2";
+	static String apiKey = "e205c73b479f0e5a66501fac3e89d86b";
+	static String apiServer = "https://dev-api.groupdocs.com/v2.0";
 	
 	public static void main(String[] args) throws Exception {
 		ApiInvoker.getInstance().setDebug(true);
@@ -33,16 +29,13 @@ public class EnvelopeExampleOne {
 				new GroupDocsRequestSigner(apiKey));
 
 		final SignatureApi api = new SignatureApi();
-		api.setBasePath("https://dev-api.groupdocs.com/v2.0");
+		api.setBasePath(apiServer);
 		
 		//upload document
-//		String filename = "interactiveform_enabled.pdf";
-//		InputStream is = EnvelopeExampleOne.class.getClassLoader()
-//				.getResourceAsStream(filename);
-		File file = new File("/home/zhaidarbek/workspace/groupdocs/java/groupdocs-java/src/test/resources/GroupDocs_Signature_Demo.pdf");
+		File file = new File(".", "src/test/resources/GroupDocs_Signature_Demo.pdf");
 		InputStream is = new FileInputStream(file);
 		StorageApi storageApi = new StorageApi();
-		storageApi.setBasePath("https://dev-api.groupdocs.com/v2.0");
+		storageApi.setBasePath(apiServer);
 		String documentId = storageApi.Upload(userId, "samples/signature/" + file.getName(), null, new FileStream(is)).getResult().getGuid();
 		IOUtils.closeQuietly(is);
 		
@@ -97,60 +90,14 @@ public class EnvelopeExampleOne {
 		envField.setPage(1);
 		api.AddSignatureEnvelopeField(userId, envelopeId, documentId, recipientId, fieldId, envField);
 		
-		// start server to handle webhook 
-//		startHttpServerForWebhook(new HttpHandler(){
-//			@Override
-//			public void handle(HttpExchange exchange) throws IOException {
-//				System.out.println("Received webhook via " + exchange.getRequestMethod());
-//				InputStream is = exchange.getRequestBody();
-//				if(is != null){
-//					String reqBody = IOUtils.toString(is);
-//					System.out.println("Request Body: " + reqBody);
-//					IOUtils.closeQuietly(is);
-//				}
-//				
-////				try{
-////					//download signed documents as archive
-////					FileStream zip = api.GetSignedEnvelopeDocuments(userId, envelopeId);
-////					FileOutputStream fos = new FileOutputStream(zip.getFileName());
-////					IOUtils.copy(zip.getInputStream(), fos);
-////					IOUtils.closeQuietly(fos);
-////					
-////					//archive envelope
-////					api.ArchiveSignatureEnvelope(userId, envelopeId);
-////				} catch(Exception e){
-////					e.printStackTrace();
-////				}
-//			}
-//		});
-		
 		//send envelope
-		FileStream stream = new FileStream(IOUtils.toInputStream("jake.dyndns.biz:8080/comparisonCallbackHandler"));
-		api.SignatureEnvelopeSend(userId, envelopeId, stream); //"jake.dyndns.biz:8080/comparisonCallbackHandler");
+		FileStream stream = new FileStream(IOUtils.toInputStream("http://jake.dyndns.biz:8080/comparisonCallbackHandler"));
+		api.SignatureEnvelopeSend(userId, envelopeId, stream); //"http://jake.dyndns.biz:8080/comparisonCallbackHandler");
 		
 		//construct embedded signature url
-		String url = "https://dev-apps.groupdocs.com/signature/sign/" + envelopeId;
+		String url = "https://dev-apps.groupdocs.com/signature/signembed/" + envelopeId + "/" + recipientId;
 		System.out.println("Please go to " + url + " and sign the envelope. Waiting for you...");
 	
-//		commented because polling is not recommended, use webhook instead
-//		
-//		//there we wait for document to be signed
-//		//assuming it's signed, we can proceed
-//		SignatureEnvelopeInfo signedEnvelope = null;
-//		do {
-//			SignatureEnvelopeResponse response = api.GetSignatureEnvelope(userId, envelopeId);
-//			if(response != null && response.getResult() != null){
-//				signedEnvelope = response.getResult().getEnvelope();
-//			}
-//		} while(signedEnvelope == null || signedEnvelope.getStatus() == 5); // completed, see http://scotland.groupdocs.com/wiki/display/SDS/envelope.status
-		
-	}
-
-	private static void startHttpServerForWebhook(HttpHandler callbackHandler) throws IOException {
-		InetSocketAddress address = new InetSocketAddress(8080);
-		HttpServer server = HttpServer.create(address, 10);
-		server.createContext("/callbackHandler", callbackHandler);
-		server.start();
 	}
 
 }
