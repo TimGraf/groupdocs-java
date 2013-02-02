7c1e565f0224c70170dab94590d970c92e1a0714
Zhaidarbek Ayazbayev <zhaidarbek@gmail.com>
2013-02-02 22:44:48 +0600

    refactor signature envelope sample into integration test

--- src/test/java/com/groupdocs/sdk/api/TestSignatureApiUseCases.java
+++ src/test/java/com/groupdocs/sdk/api/TestSignatureApiUseCases.java
@@ -1,5 +1,10 @@
 package com.groupdocs.sdk.api;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -7,49 +12,46 @@ import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Test;
 
-import com.groupdocs.sdk.common.ApiInvoker;
 import com.groupdocs.sdk.common.FileStream;
-import com.groupdocs.sdk.common.GroupDocsRequestSigner;
 import com.groupdocs.sdk.model.SignatureEnvelopeDocumentResponse;
 import com.groupdocs.sdk.model.SignatureEnvelopeFieldSettings;
 import com.groupdocs.sdk.model.SignatureEnvelopeResponse;
 import com.groupdocs.sdk.model.SignatureEnvelopeSettings;
 import com.groupdocs.sdk.model.SignatureFieldInfo;
 import com.groupdocs.sdk.model.SignatureRoleInfo;
+import com.groupdocs.sdk.model.SignatureStatusResponse;
 
-public class EnvelopeExampleOne {
-	static String userId = "16698cae909805f2";
-	static String apiKey = "e205c73b479f0e5a66501fac3e89d86b";
-	static String apiServer = "https://dev-api.groupdocs.com/v2.0";
+public class TestSignatureApiUseCases extends AbstractIntegrationTest {
 	
-	public static void main(String[] args) throws Exception {
-		ApiInvoker.getInstance().setDebug(true);
-		ApiInvoker.getInstance().setRequestSigner(
-				new GroupDocsRequestSigner(apiKey));
-
-		final SignatureApi api = new SignatureApi();
-		api.setBasePath(apiServer);
-		
+	SignatureApi api = new SignatureApi();
+	
+	@Test
+	public void testSendEnvelopeForSigning() throws Exception {
 		//upload document
-		File file = new File(".", "src/test/resources/GroupDocs_Signature_Demo.pdf");
+		File file = new File(".", "src/test/resources/interactiveform_enabled.pdf");
 		InputStream is = new FileInputStream(file);
 		StorageApi storageApi = new StorageApi();
-		storageApi.setBasePath(apiServer);
 		String documentId = storageApi.Upload(userId, "samples/signature/" + file.getName(), null, new FileStream(is)).getResult().getGuid();
 		IOUtils.closeQuietly(is);
+		assertThat(documentId, not(nullValue()));
 		
 		//create envelope
 		SignatureEnvelopeSettings env = new SignatureEnvelopeSettings();
 		env.setEmailSubject("Sing this!");
 		SignatureEnvelopeResponse envelopeResponse = api.CreateSignatureEnvelope(userId, "SampleEnvelope_" + UUID.randomUUID(), env, null, null);
+		assertThat(envelopeResponse, not(nullValue()));
 		
 		//add document to envelope
 		final String envelopeId = envelopeResponse.getResult().getEnvelope().getId();
+		assertThat(envelopeId, not(nullValue()));
 		SignatureEnvelopeDocumentResponse envelopeDocument = api.AddSignatureEnvelopeDocument(userId, envelopeId, documentId, null);
+		assertThat(envelopeDocument, not(nullValue()));
 		
 		//update document object after it's created
 		documentId = envelopeDocument.getResult().getDocument().getDocumentId();
+		assertThat(documentId, not(nullValue()));
 		
 		//add new recipient to envelope
 		String roleId = null;
@@ -59,17 +61,19 @@ public class EnvelopeExampleOne {
 				roleId = role.getId();
 			}
 		}
-		String recipientId = api.AddSignatureEnvelopeRecipient(userId, envelopeId, "jake.ayazbayev@gmail.com", "Jake", "Ayazbayev", null, roleId).getResult().getRecipient().getId();
+		assertThat(roleId, not(nullValue()));
+		String recipientId = api.AddSignatureEnvelopeRecipient(userId, envelopeId, "john@smith.com", "John", "Smith", null, roleId).getResult().getRecipient().getId();
+		assertThat(recipientId, not(nullValue()));
 		
 		//add city field to envelope
 		String fieldId = null;
 		List<SignatureFieldInfo> fields = api.GetFieldsList(userId, null).getResult().getFields();
 		for(SignatureFieldInfo field : fields){
-			System.out.println(field);
 			if(field.getFieldType() == 2){ // single line, see http://scotland.groupdocs.com/wiki/display/SDS/field.type
 				fieldId = field.getId();
 			}
 		}
+		assertThat(fieldId, not(nullValue()));
 		SignatureEnvelopeFieldSettings envField = new SignatureEnvelopeFieldSettings();
 		envField.setName("City");
 		envField.setLocationX(0.3);
@@ -85,19 +89,20 @@ public class EnvelopeExampleOne {
 				fieldId = field.getId();
 			}
 		}
+		assertThat(fieldId, not(nullValue()));
+		envField.setName("Signature");
 		envField.setLocationX(0.3);
 		envField.setLocationY(0.3);
 		envField.setPage(1);
 		api.AddSignatureEnvelopeField(userId, envelopeId, documentId, recipientId, fieldId, envField);
 		
 		//send envelope
-		FileStream stream = new FileStream(IOUtils.toInputStream("http://jake.dyndns.biz:8080/comparisonCallbackHandler"));
-		api.SignatureEnvelopeSend(userId, envelopeId, stream); //"http://jake.dyndns.biz:8080/comparisonCallbackHandler");
+		SignatureStatusResponse envelopeSend = api.SignatureEnvelopeSend(userId, envelopeId, null);
+		assertThat(envelopeSend, not(nullValue()));
+		assertThat(envelopeSend.getStatus(), equalTo("Ok"));
 		
-		//construct embedded signature url
-		String url = "https://dev-apps.groupdocs.com/signature/signembed/" + envelopeId + "/" + recipientId;
-		System.out.println("Please go to " + url + " and sign the envelope. Waiting for you...");
-	
+		String embedUrl = "https://apps.groupdocs.com/signature/signembed/" + envelopeId + "/" + recipientId;
+		System.out.println("Use following URL to sign the envelope: " + embedUrl);
 	}
 
 }
